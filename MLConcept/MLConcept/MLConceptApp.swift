450354950623b55f5957350da58b136d62d69841
RosasSebastian2003 <rosassebastian2003@gmail.com>
2024-02-26 12:12:31 -0600

    Trying to push changes

---
+++ MLConcept/MLConcept/MLConceptApp.swift
@@ -0,0 +1,17 @@
+//
+//  MLConceptApp.swift
+//  MLConcept
+//
+//  Created by Sebastian Rosas Maciel on 2/26/24.
+//
+
+import SwiftUI
+
+@main
+struct MLConceptApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
