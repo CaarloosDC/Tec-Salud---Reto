f106330137af950a173e76b2f493f9cc811ee672
Carlos <A00836623@tec.mx>
2024-02-29 20:29:01 -0600

    First Vision Pro App in repo

---
+++ Reto-Tec-Salud/Reto-Tec-Salud/Reto_Tec_SaludApp.swift
@@ -0,0 +1,17 @@
+//
+//  Reto_Tec_SaludApp.swift
+//  Reto-Tec-Salud
+//
+//  Created by Carlos DC on 29/02/24.
+//
+
+import SwiftUI
+
+@main
+struct Reto_Tec_SaludApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
