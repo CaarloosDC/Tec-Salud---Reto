53da127f07c73fa8b98a2476589df897a959aacb
Sebastian Rosas Maciel <rosassebastian2003@gmail.com>
2024-03-14 14:01:59 -0600

    structure change

--- Reto-Tec-Salud/Reto-Tec-Salud/Foundation/Data Models/BodyPart.swift
+++ Reto-Tec-Salud/Reto-Tec-Salud/Foundation/Data Models/BodyPart.swift
@@ -8,18 +8,18 @@
 import Foundation
 
 
-struct BodyPart: Identifiable, Codable {
+struct BodyPart: Identifiable, Codable, Hashable {
     let id, medicalName, imageName, renderName: String
     let doableProcedures: [Procedure]
 }
 
-struct Procedure: Identifiable, Codable {
+struct Procedure: Identifiable, Codable, Hashable {
     let id: Int
     let surgeryTechnicalName, description: String
     let steps: [Step]
 }
 
-struct Step: Identifiable, Codable {
+struct Step: Identifiable, Codable, Hashable {
     let id: Int
     let description, imageName: String
 }
