ec0f393bd50ad40a35782cee3f759238aea34e60
Carlos <A00836623@tec.mx>
2024-04-10 20:54:35 -0600

    Imagefield added to the API

--- API/MedVision/models.py
+++ API/MedVision/models.py
@@ -3,9 +3,9 @@ from django.db import models
 # Create your models here.
 
 class BodyPart(models.Model):
-    id = models.CharField(primary_key=True, max_length=7)
+    id = models.CharField(primary_key=True, max_length=20)
     medicalName = models.CharField(max_length=25)
-    imageName = models.CharField(max_length=255) # Cambiar por imagen
+    image = models.ImageField(upload_to='media/images/')
     renderName = models.CharField(max_length=255)
 
     def __str__(self):
@@ -31,3 +31,4 @@ class Step(models.Model):
 
     def __str__(self):
         return f"{self.order}. {self.description[:50]}"  # Muestra el orden y parte de la descripción
+    
--- API/MedVision/serializer.py
+++ API/MedVision/serializer.py
@@ -2,20 +2,23 @@ from rest_framework import serializers
 from .models import BodyPart, DoableProcedure, Step
 
 class StepSerializer(serializers.ModelSerializer):
+    image = serializers.ImageField(use_url=True, required=False)
+
     class Meta:
         model = Step
-        fields = ['description', 'imageName', 'order']
+        fields = ['id', 'description', 'image', 'order', 'shortDescription']
+
 
 class DoableProcedureSerializer(serializers.ModelSerializer):
-    steps = StepSerializer(many=True, read_only=True)  # Asegúrate de que 'steps' es el related_name en el modelo
+    steps = StepSerializer(many=True, read_only=True)
 
     class Meta:
         model = DoableProcedure
         fields = ['pk','surgeryTechnicalName', 'description', 'steps']
 
 class BodyPartSerializer(serializers.ModelSerializer):
-    procedures = DoableProcedureSerializer(many=True, read_only=True)  # 'procedures' es el related_name en el modelo
-
+    # procedures = DoableProcedureSerializer(many=True, read_only=True)  # 'procedures' es el related_name en el modelo
+    image = serializers.ImageField(use_url=True)
     class Meta:
         model = BodyPart
-        fields = ['id', 'medicalName', 'imageName', 'renderName', 'procedures']
+        fields = ['id', 'medicalName', 'image', 'renderName']
