721599def78dd32e75769907f86c2a101ff9ae78
Sebastian Rosas Maciel <rosassebastian2003@gmail.com>
2024-04-12 01:08:24 -0600

    Enum for content type display added, ornament as well as detail text should update

---
+++ Reto-Tec-Salud/Reto-Tec-Salud/Views/Windows/SurgeryDetailContentWindow.swift
@@ -0,0 +1,18 @@
+//
+//  SurgeryDetailContentWindow.swift
+//  Reto-Tec-Salud
+//
+//  Created by Sebastian Rosas Maciel on 4/11/24.
+//
+
+import SwiftUI
+
+struct SurgeryDetailContentWindow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SurgeryDetailContentWindow()
+}
