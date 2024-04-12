9512293c19ab6d36750b8acab978c6b53ca2d827
Sebastian Rosas Maciel <rosassebastian2003@gmail.com>
2024-04-11 21:28:45 -0600

    Transmiter frontend pimped

---
+++ TecMedTransmiter/TecMedTransmiter/Views/Components/LoadingView.swift
@@ -0,0 +1,18 @@
+//
+//  LoadingView.swift
+//  TecMedTransmiter
+//
+//  Created by Sebastian Rosas Maciel on 4/11/24.
+//
+
+import SwiftUI
+
+struct LoadingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoadingView()
+}
