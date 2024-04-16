8ae0c1379f2ddef8fb60c10d3e12ddfb54f65ae4
Roberto Santos Moises <23622221+shapm@users.noreply.github.com>
2024-04-16 13:50:58 -0600

    Cambio a FastAPI
    En caso de que falten paquetes hay que intalar:
    pipenv install fastapi supabase "uvicorn[standard]"

--- API/MedVision/views.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import viewsets
-from .models import BodyPart, DoableProcedure, Step
-from .serializer import BodyPartSerializer
-
-# Create your views here.
-class BodyPartViewSet(viewsets.ModelViewSet):
-    serializer_class = BodyPartSerializer
-    queryset = BodyPart.objects.all()
